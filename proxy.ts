13cc29514a10cd87acf1aeff3d9c458533551b7e
dorian <zdorianyael@gmail.com>
2026-09-03 11:12:50 -0600

    Arreglo pwa

--- proxy.ts
+++ proxy.ts
@@ -38,6 +38,6 @@ export function proxy(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|manifest.json|.*\\.png$|.*\\.svg$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|manifest.json|sw\\.js|workbox-.*|.*\\.png$|.*\\.svg$).*)',
   ],
 }

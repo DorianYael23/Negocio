044093a3484b6848991e7e58ce43b661fc2f0b0d
dorian <zdorianyael@gmail.com>
2026-09-03 10:32:44 -0600

    Cambios pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // Evita problemas de caché mientras programas en local
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -6,6 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default withPWA(nextConfig);
